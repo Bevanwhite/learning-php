879d8037a908b37210fe9ddc3398d3f8130dc7a1
bevan <jeffery1996.jbw@gmail.com>
2021-02-22 23:03:31 +0530

    if-else statements

---
+++ if-else.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>  
+    <?php
+        if (3 > 10){
+            echo "three is less than ten";
+        } elseif(3 < 4){
+            echo "of course four is gratter than three";
+        } else {
+            echo "shut off";
+        };
+    ?>
+</body>
+</html>
