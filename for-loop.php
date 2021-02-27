50663cfaa08a96a435dc46c4ebde4d53ebe21228
bevan <jeffery1996.jbw@gmail.com>
2021-02-27 11:30:45 +0530

    for and while loop

---
+++ for-loop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    for($number =0; $number < 10; $number++){
+        echo "this is fucking awsome"." ". $number+1 ."<br>"; 
+    }
+
+?>
+</body>
+</html>
---
+++ whileloop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $x = 0;
+    // created a while loop to display some numbers
+    while($x < 10) {
+        echo $x+1 ." "."this is it"."<br>";
+        $x+=1;
+    };
+
+?>
+</body>
+</html>
