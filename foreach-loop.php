e83831015e6a0c7196b026dc21977800e301afa8
bevan <jeffery1996.jbw@gmail.com>
2021-02-27 12:03:26 +0530

    for each loop

---
+++ foreach-loop.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $numbers = array(20,36,463,5464,7984,44121,6446,4452,459,456);
+    // it allow us to display number as a children in the array
+    foreach($numbers as $number){
+        echo $number. "<br>";
+    }
+
+?>
+</body>
+</html>
